e3f8e85ef62d2fe92e6b2495ef48c2ecc6e1244f
Elvis Knapman <elvis@elvisknapman.com>
2019-09-03 15:44:03 -0400

    :sparkles: wrote POST request to add new posts

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -15,4 +15,27 @@ router.get("/", (req, res) => {
     });
 });
 
+// create new post
+router.post("/", (req, res) => {
+  const newPost = req.body;
+
+  if (newPost.title && newPost.contents) {
+    db.insert(newPost)
+      .then(result => {
+        res.status(201).json(result);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        });
+      });
+  } else {
+    res
+      .status(400)
+      .json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+  }
+});
+
 module.exports = router;
