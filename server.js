ff8be717384d5be15a85012cc7fc29d8bd2396fe
Elvis Knapman <elvis@elvisknapman.com>
2019-09-03 15:37:11 -0400

    :sparkles: added route for posts (/api/posts/)

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const postRoutes = require("./posts/postRoutes.js");
 
 const server = express();
 
 server.use(express.json());
+server.use("/api/posts", postRoutes);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up and running..." });
+});
 
 module.exports = server;
