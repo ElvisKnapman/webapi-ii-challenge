7ffbd8999bd3170b58d3a280230f5197a6899bb4
Elvis Knapman <elvis@elvisknapman.com>
2019-09-03 15:36:34 -0400

    :recycle: changed port, added main route

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const server = require("./server.js");
 
-const port = 8500;
-
-server.get("/", (req, res) => {
-  res.status(200).json({ api: "up and running..." });
-});
+const port = 8100;
 
 server.listen(port, () => {
   console.log(`\nserver up and running on port ${port}\n`);
